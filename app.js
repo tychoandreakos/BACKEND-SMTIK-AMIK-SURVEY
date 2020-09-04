663d1309c16859eeeb768e64c9b3daedb1325fc0
tychoandreakos <elangindra15@gmail.com>
2020-09-04 06:24:28 +0000

    adding bodyParser

--- app.js
+++ app.js
@@ -2,10 +2,11 @@ const express = require("express");
 const app = express();
 const port = 8000;
 const db = require("./Configuration");
+const bodyParser = require("body-parser");
 
 const Router = require("./Router");
 db;
 
-app.use([Router]);
+app.use([Router, bodyParser.json()]);
 
 app.listen(port, () => console.log(`Connected to port ${port}`));
